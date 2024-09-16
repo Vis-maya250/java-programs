3c52fa256dd6d52b5e15cbe3ceae5a27ffc44342
Vis_maya_ <vismayavinod250@gmail.com>
2024-09-16 21:19:49 +0530

    Charfrequency.java

---
+++ Charfrequency.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Frequency
+  {
+     public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the string: ");
+      String str=sc.nextLine();
+      System.out.println("Enter the character: ");
+      char ch=sc.next().charAt(0);
+      int count=0;
+      int len=str.length();
+      for(int i=0;i<len;i++)
+      {
+          if(str.charAt(i)==ch)
+          {
+              count++;
+          }
+      }
+      System.out.println("Frequency of the character is: "+count);
+    }
+  }
