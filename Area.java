27f271663aaaa080962952f43dfa761de39c468f
Vis_maya_ <vismayavinod250@gmail.com>
2024-09-16 21:05:42 +0530

    Area.java

--- Area.java
+++ Area.java
@@ -4,7 +4,7 @@ public class Area
      public static void main(String args[])
     {
       Scanner sc=new Scanner(System.in);
-      System.out,println("Enter the length of the rectangle: ");
+      System.out.println("Enter the length of the rectangle: ");
       int l=sc.nextInt();
       System.out.println("Enter the breadth of the rectangle: ");
       int b=sc.nextInt();
