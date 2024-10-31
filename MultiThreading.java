47116cfd0db699524ade505c47ed8a3f9ac9ef20
Vis_maya_ <vismayavinod250@gmail.com>
2024-10-31 12:09:58 +0530

    MultiThreading.java

---
+++ MultiThreading.java
@@ -0,0 +1,67 @@
+import java.util.*;
+class EvenNum implements Runnable
+{
+	public int a;
+	public EvenNum(int a)
+	{
+		this.a=a;
+	}
+	public void run()
+	{    
+		System.out.println("The Thread "+a+ " is EVEN and Square of "+a+" is: "+a*a);
+	}
+	}
+
+class OddNum implements Runnable
+{
+	public int a;
+	public OddNum(int a)
+	{
+		this.a=a;
+	}
+	public void run()
+	{
+		System.out.println("The Thread "+a+ " is ODD and Cube of "+a+" is: "+a*a*a);
+	}
+}
+
+class RandomNumGenerator extends Thread
+{
+	public void run()
+	{
+		int n=0;
+		Random rand=new Random();
+		try
+		{
+			for(int i=0;i<5;i++)
+			{
+				n=rand.nextInt(20);
+				System.out.println("Generated number is "+n);
+				if(n%2==0)
+				{
+					Thread thread1=new Thread(new EvenNum(n));
+					thread1.start();
+				}
+				else
+				{
+					Thread thread2=new Thread(new OddNum(n));
+					thread2.start();
+				}
+				Thread.sleep(1000);
+				System.out.println("...................");
+			}
+		}
+		catch(Exception ex)
+		 {
+			System.out.println (ex.getMessage());
+		}
+	}
+}
+public class MultiThreadRandOddEven 
+{
+    public static void main(String args[])
+     {
+    	 RandomNumGenerator randnum=new RandomNumGenerator();
+    	 randnum.start();
+     }
+}
